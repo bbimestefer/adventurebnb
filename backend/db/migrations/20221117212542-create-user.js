8a36a502395c029a3926c14311fa9a80d07a6b1b
brandon <brandon.bim13@gmail.com>
2022-11-17 16:55:12 -0500

    Add Users table migration and seeds

---
+++ backend/db/migrations/20221117212542-create-user.js
@@ -0,0 +1,60 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(256)
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    }, options);
+    // adds the index
+    await queryInterface.addIndex(
+      'Users',
+      ['username', 'email'],
+      {
+        unique: true
+      }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users', options);
+    // removes the index
+    await queryInterface.removeIndex(
+      'Users',
+      ['username', 'email'],
+    );
+  }
+};
