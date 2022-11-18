4f6000b7af359bd4a8b21116da88cd355cf08326
brandon <brandon.bim13@gmail.com>
2022-11-18 16:16:13 -0500

    changing migration file

--- backend/db/migrations/20221117212542-create-user.js
+++ backend/db/migrations/20221117212542-create-user.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA;
 }
 
-// /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,45 +21,32 @@ module.exports = {
         type: Sequelize.STRING
       },
       username: {
+        type: Sequelize.STRING(30),
         allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(30)
+        unique: true
       },
       email: {
+        type: Sequelize.STRING(256),
         allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(256)
+        unique: true
       },
       hashedPassword: {
-        allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     }, options);
-    // adds the index
-    await queryInterface.addIndex(
-      'Users',
-      ['username', 'email'],
-      {
-        unique: true
-      }
-    );
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users', options);
-    // removes the index
-    await queryInterface.removeIndex(
-      'Users',
-      ['username', 'email'],
-    );
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Users", options);
   }
 };
