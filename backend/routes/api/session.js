b19f54d508e4931d54ee47638c741be713c6a97f
brandon <brandon.bim13@gmail.com>
2022-11-18 12:46:19 -0500

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,7 +3,22 @@ const express = require('express')
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+// checks to see whether or not req.body.credential and req.body.password are empty
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 // Restore session user
 router.get(
@@ -19,26 +34,26 @@ router.get(
   }
 );
 
-router.post('/', async (req, res, next) => {
-      const { credential, password } = req.body;
+router.post('/', validateLogin, async (req, res, next) => {
+    const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+    const user = await User.login({ credential, password });
 
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
-
-      await setTokenCookie(res, user);
-
-      return res.json({
-        user
-      });
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
     }
-  );
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user: user
+    });
+  }
+);
 
 router.delete('/', (_req, res) => {
       res.clearCookie('token');
