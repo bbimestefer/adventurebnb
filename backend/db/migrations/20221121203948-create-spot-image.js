afad12bd7e2279a6b9be54482b6021b509b21e6b
brandon <brandon.bim13@gmail.com>
2022-11-21 16:36:55 -0500

    Added migration and model files then migrated

---
+++ backend/db/migrations/20221121203948-create-spot-image.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('SpotImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Spots', id: 'id' },
+        onDelete: 'CASCADE',
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      preview: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    }, options);
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('SpotImages', options);
+  }
+};
