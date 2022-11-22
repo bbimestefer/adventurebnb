33ed3f7ef0f4307069a2bdd435fbb6711d07661f
brandon <brandon.bim13@gmail.com>
2022-11-21 22:51:23 -0500

    Added seeder for reviews

--- backend/db/seeders/20221122034127-reviews.js
+++ backend/db/seeders/20221122034127-reviews.js
@@ -7,34 +7,40 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    options.tableName = 'Bookings';
+    options.tableName = 'Reviews';
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
         userId: 1,
-        startDate: '2023-07-12',
-        endDate: '2023-07-19',
+        review: 'Great place to see the mountains',
+        stars: 4,
       },
       {
         spotId: 3,
-        userId: 1,
-        startDate: '2023-08-02',
-        endDate: '2023-08-10',
+        userId: 3,
+        review: 'my sons name is jackson',
+        stars: 5,
       },
       {
         spotId: 2,
         userId: 1,
-        startDate: '2023-11-23',
-        endDate: '2023-11-27',
+        review: 'this place is hot',
+        stars: 3,
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        review: 'could be prettier',
+        stars: 2,
       },
     ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    options.tableName = 'Bookings';
+    options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      startDate: { [Op.in]: ['2023-07-12', '2023-08-02', '2023-11-23'] }
+      userId: { [Op.in]: [1, 2, 3] }
     }, {});
   }
 };
