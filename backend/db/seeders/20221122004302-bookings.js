0dc0d2322d0cf1b76d5722f3efbab22d7b0e628a
brandon <brandon.bim13@gmail.com>
2022-11-21 20:04:39 -0500

    Created bookings seeder and index

---
+++ backend/db/seeders/20221122004302-bookings.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Bookings';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: '2023-07-12',
+        endDate: '2023-07-19',
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        startDate: '2023-08-02',
+        endDate: '2023-08-10',
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        startDate: '2023-11-23',
+        endDate: '2023-11-27',
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      startDate: { [Op.in]: ['2023-07-12', '2023-08-02', '2023-11-23'] }
+    }, {});
+  }
+};
