052e55b173886bb005c99687f2d86c28140966ce
brandon <brandon.bim13@gmail.com>
2022-11-23 12:12:23 -0500

    Added bookings router

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+const sequelize = require('sequelize');
+const { requireAuth } = require("../../utils/auth.js");
+const { User, Spot, Booking, SpotImage, ReviewImage, Review } = require('../../db/models');
+
+router.get('/current', requireAuth, async (req, res, next) => {
+    const userId = req.user.id
+
+    const bookingsOfUser = await Booking.findAll({
+        where: {
+            userId
+        }
+    })
+
+    res.json(bookingsOfUser)
+})
+
+module.exports = router;
