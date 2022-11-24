17267e629981d5a27d6725c0663c15d6e13d5cfe
brandon <brandon.bim13@gmail.com>
2022-11-23 19:21:30 -0500

    Finished Editing a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -47,4 +47,58 @@ router.get('/current', requireAuth, async (req, res, next) => {
     })
 })
 
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    const { startDate, endDate } = req.body
+    const dateStart = new Date(startDate)
+    const dateEnd = new Date(endDate)
+    const booking = await Booking.findByPk(req.params.bookingId)
+
+    const bookingStartDate = new Date(booking.startDate)
+    const bookingEndDate = new Date(booking.endDate)
+
+    const today = new Date()
+
+    if(!booking){
+        res.status(404)
+        res.json({
+            "message": "Booking couldn't be found",
+            "statusCode": 404
+          })
+    } else if(today.getTime() >= bookingEndDate.getTime()) {
+        res.status(403)
+        res.json({
+            "message": "Past bookings can't be modified",
+            "statusCode": 403
+          })
+    } else if(dateStart.getTime() >= bookingStartDate.getTime() && dateStart.getTime() <= bookingEndDate.getTime() ||
+    dateEnd.getTime() >= bookingStartDate.getTime() && dateEnd.getTime() <= bookingEndDate.getTime() ||
+    dateStart.getTime() <= bookingStartDate.getTime() && dateEnd.getTime() >= bookingEndDate.getTime()) {
+        res.status(403)
+        res.json({
+            "message": "Sorry, this spot is already booked for the specified dates",
+            "statusCode": 403,
+            "errors": {
+            "startDate": "Start date conflicts with an existing booking",
+            "endDate": "End date conflicts with an existing booking"
+            }
+        })
+    } else if(booking.userId !== req.user.id){
+        res.status(403)
+        res.json({
+            "message": "Forbidden",
+            "statusCode": 403
+        })
+    }
+
+    booking.set({
+        startDate,
+        endDate
+    })
+
+    await booking.save()
+
+    res.json(booking)
+})
+
+
 module.exports = router;
