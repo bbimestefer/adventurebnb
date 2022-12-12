bb739ba0f501aede99b51f5e6a5f0dcab3e7c79a
brandon <brandon.bim13@gmail.com>
2022-12-12 17:25:32 -0500

    working on single spot details

--- frontend/src/components/Spots/SingleSpot.js
+++ frontend/src/components/Spots/SingleSpot.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import * as spotActions from '../../store/spots'
+import "./SingleSpot.css"
 
 function SingleSpot () {
     const dispatch = useDispatch()
@@ -11,11 +12,35 @@ function SingleSpot () {
         dispatch(spotActions.getSpotById(id))
     }, [id, dispatch])
 
+    const spot = useSelector(state => state.spots.singleSpot)
 
-
+    if(!spot) return null
     return (
-        <div>
-            <p>This should be the page im redirected to {id}</p>
+        <div className="wrapper-for-info">
+            <div className="header">
+                <h2 className="text spot-header">
+                    {spot.name}
+                </h2>
+                <div className="sub-info">
+                    <div className='ratings'>
+                        <span><i className="fa-sharp fa-solid fa-star"></i>{spot.avgStarRating}</span>
+                        <li>{spot.numReviews} reviews</li>
+                        <li>{spot.city}, {spot.state}, {spot.country}</li>
+                    </div>
+                    <div className="share">
+                        <span><i class="fa-solid fa-arrow-up-from-bracket"></i> Share</span>
+                        <span><i className="fa-regular fa-heart"></i> Save</span>
+                    </div>
+                </div>
+            </div>
+            <div>WHERE PICTURES GO</div>
+            <div className="details">
+                <div className="sub-info">
+                    <h3 className="host">This home hosted by </h3>
+                    <div>pic</div>
+                </div>
+                <div className="reserve-form">reserve form</div>
+            </div>
         </div>
     )
 }
