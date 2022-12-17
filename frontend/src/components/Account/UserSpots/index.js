9834c41867d12b682f780383c419bbb005ed3cab
brandon <brandon.bim13@gmail.com>
2022-12-16 20:01:58 -0500

    added dynamic review and spot showing and nav-bar css

--- frontend/src/components/Account/UserSpots/index.js
+++ frontend/src/components/Account/UserSpots/index.js
@@ -21,9 +21,12 @@ export default function SpotsOfUser() {
     return (
         <div style={{"margin":"40px", "padding":"0px 10px", "display":"flex", "flexDirection":"column", "border":"lightGray solid 1px", "borderRadius":"10px"}}>
             <h2 style={{"borderBottom":"solid lightgray 1px", "padding":"10px"}}>Your Spots:</h2>
-            {userSpots.map((spot) => (
-                <UserSpotDetails key={spot.id} {...spot} />
-                ))}
+            {userSpots.length ? (
+                userSpots.map((spot) => (
+                    <UserSpotDetails key={spot.id} {...spot} />
+                    ))
+            ) : (<h5>No spots listed.</h5>)
+            }
         </div>
 
     )
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -11,20 +11,22 @@ function Navigation({ isLoaded }){
 
   return (
     <div className='nav-bar-wrapper'>
-      <div>
-      <NavLink exact to="/" className='home-link'><img style={{"height":"25px", "width":"25px"}} src={logo} alt='logo'></img> adventurebnb</NavLink>
-      </div>
-      <div style={{"width":"50px"}}></div>
-      {isLoaded && (
-        <div style={{"display": 'flex', 'alignItems': 'center', 'gap':'20px'}}>
-          <div>
-            { sessionUser && (
-              <NavLink to='/new' style={{"textDecoration":"underline"}}>adventurebnb your home</NavLink>
-            )}
-          </div>
-          <ProfileButton user={sessionUser} />
+      <div className='nav-bar'>
+        <div>
+        <NavLink exact to="/" className='home-link'><img style={{"height":"25px", "width":"25px"}} src={logo} alt='logo'></img> adventurebnb</NavLink>
         </div>
-      )}
+        <div style={{"width":"50px"}}></div>
+        {isLoaded && (
+          <div style={{"display": 'flex', 'alignItems': 'center', 'gap':'20px'}}>
+            <div>
+              { sessionUser && (
+                <NavLink to='/new' style={{"textDecoration":"underline"}}>adventurebnb your home</NavLink>
+              )}
+            </div>
+            <ProfileButton user={sessionUser} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
